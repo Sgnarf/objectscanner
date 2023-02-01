d2c677ffdbe42b79ad5d91e99db5067c6472927e
Sgnarf <93402593+Sgnarf@users.noreply.github.com>
2023-02-01 20:05:44 +0100

    Update and rename camera_initialize.py to Arducam_OV5642_mp.py

---
+++ camera/Arducam_OV5642_mp.py
@@ -0,0 +1,1 @@
+#Based on https://github.com/teco-kit/Arducam_OV2640_Python_Package_Raspberry_Pi_Pico/blob/main/Pico/Code/Arducam_OV2640.py but rewritten to use standard libraries instead of AdaFruit ones
